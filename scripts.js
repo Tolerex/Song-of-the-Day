bf847c914526a45ca2d410f26a04bdb1874c053a
Tolerex <130232510+JulienToumieh1@users.noreply.github.com>
2023-05-14 23:12:25 +0300

    no message

--- scripts.js
+++ scripts.js
@@ -8,9 +8,11 @@ function provider(value){
     }
     if (value == 2){
         document.getElementById('player').src = "https://www.youtube.com/embed/9VBU-1Lynho";
+        document.getElementById('player').src = "https://widget.anghami.com/song/1109405998/?theme=fulldark&layout=wide&lang=en";
     }
     if (value == 3){
         document.getElementById('player').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/337048167&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
     }
 }
 
+  
